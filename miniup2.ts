b56dcc4f6b5df454c52a550b19926580759651bf
mweststrate <michel@mweststrate.nl>
2013-08-17 13:43:10 +0200

    - added notes and test cases about left recursion - added bootstrap parser for parsing miniup grammars - minor code improvements

--- miniup2.ts
+++ miniup2.ts
@@ -19,14 +19,22 @@ module miniup {
 		//TODO: $ matcher (what does it mean?) ->Try to match the expression. If the match succeeds, return the matched string instead of the match result.
 		//TODO: import matcher (requires global registry)
 		//TODO: operator matcher / recursion matcher
+		//TODO: non lazy or matcher?
 
-		public static regexMatcher(regex: string, ignoreCase: bool = false): ParseFunction {
-			var r = new RegExp("\\A" + regex, ignoreCase ? "i" : "");
+		public static regex(regex: RegExp, ignoreCase: bool = false, autoWhitespace: bool = true): ParseFunction {
+			//TODO: avoid whitespace matcher to auto match whitespace!
+			var r = new RegExp("\\A" + regex.source, ignoreCase ? "i" : "");
 
 			return (parser: Parser): any => {
 				var remainingInput = parser.getRemainingInput();
+				if (autoWhitespace)
+					parser.consumeWhitespace();
+
 				var match = remainingInput.match(r);
 				if (match) {
+					if (autoWhitespace) //TODO: nowhere post whitespace matching?
+						parser.consumeWhitespace();
+
 					parser.currentPos += match[0].length;
 					return match[0];
 				}
@@ -34,30 +42,25 @@ module miniup {
 			}
 		}
 
-		public static characterMatcher(regex: string, ignoreCase: bool = false): ParseFunction {
-			return regexMatcher(regex, ignoreCase);
+		public static characterClass(regexstr: string, ignoreCase: bool = false): ParseFunction {
+			return regex(new RegExp(regexstr), ignoreCase, false);
 		}
 
-		public static keywordMatcher(keyword: string, ignoreCase: bool = false): ParseFunction {
-			var rem = regexMatcher("\\A" + Util.quoteRegExp(keyword), ignoreCase);
-			return Util.extend((parser: Parser): any => {
-				parser.consumeWhitespace();
-				var res = parser.parse(rem);
-				parser.consumeWhitespace();
-				return res;
-			}, {
-				isKeywordMatcher: true
-			});
+		public static literal(keyword: string, ignoreCase: bool = false): ParseFunction {
+			return Util.extend(
+				regex(new RegExp("\\A" + RegExpUtil.quoteRegExp(keyword)), ignoreCase, true),
+				{ isKeywordMatcher: true }
+			);
 		}
 
-		public static ruleMatcher(ruleName: string): ParseFunction {
+		public static rule(ruleName: string): ParseFunction {
 			return (parser: Parser): any => {
 				return Util.extend(parser.parse(parser.grammar.rule(ruleName)), { ruleName: ruleName });
 				//TODO: automatically set rulename property?
 			}
 		}
 
-		public static zeroOrMoreMatcher(matcher: ParseFunction): ParseFunction {
+		public static zeroOrMore(matcher: ParseFunction): ParseFunction {
 			return (parser: Parser): any => {
 				var res = [];
 				var item;
@@ -71,8 +74,8 @@ module miniup {
 			}
 		}
 
-		public static oneOrMoreMatcher(matcher: ParseFunction): ParseFunction {
-			var zmm = zeroOrMoreMatcher(matcher);
+		public static oneOrMore(matcher: ParseFunction): ParseFunction {
+			var zmm = zeroOrMore(matcher);
 
 			return (parser: Parser): any => {
 				var res = parser.parse(zmm);
@@ -80,15 +83,15 @@ module miniup {
 			}
 		}
 
-		public static zeroOrOneMatcher(matcher: ParseFunction): ParseFunction {
+		public static optional(matcher: ParseFunction): ParseFunction {
 			return (parser: Parser): any => {
 				var res = parser.parse(matcher);
 				return res === undefined ? null : res;
 			}
 		}
 
-		public static sequenceMatcher(items: ParseFunction[]): ParseFunction {
-			if (items.length == 1) //Not a real sequence
+		public static sequence(...items: ParseFunction[]): ParseFunction {
+			if (items.length == 1 && !items[0].ruleName) //Not top level, and not a real sequence
 				return items[0];
 
 			return (parser: Parser): any => {
@@ -101,6 +104,7 @@ module miniup {
 
 					if (item.isKeywordMatcher && !item.friendlyName && items.length > 1)
 						return true; //ignore result if it is just a stringmathcer without astname. Keywords are not interesting in an AST
+					//TODO: ignore result if its a lookAhead
 
 					result.push(itemres);
 					if (item.astName)
@@ -113,7 +117,7 @@ module miniup {
 			}
 		}
 
-		public static choiceMatcher(choices: ParseFunction[]): ParseFunction {
+		public static choice(choices: ParseFunction[]): ParseFunction {
 			return (parser: Parser): any => {
 				var res;
 
@@ -123,7 +127,7 @@ module miniup {
 			}
 		}
 
-		public static positivePredicateMatcherMatcher(predicate: ParseFunction): ParseFunction {
+		public static positiveLookAhead(predicate: ParseFunction): ParseFunction {
 			return (parser: Parser): any => {
 				var prepos = parser.currentPos;
 				//TODO: do *not* update best match while parsing predicates!
@@ -133,17 +137,20 @@ module miniup {
 			}
 		}
 
-		public static negativePredicateMatcherMatcher(predicate: ParseFunction): ParseFunction {
-			var ppm = positivePredicateMatcherMatcher(predicate);
+		public static negativeLookAhead(predicate: ParseFunction): ParseFunction {
+			var ppm = positiveLookAhead(predicate);
 			return (parser: Parser): any => {
 				return parser.parse(ppm) === undefined ? null : undefined; //undefined == no match. null == match, so invert.
 			}
 		}
 
-		public static namedRule(matcher: ParseFunction, names: NamedRule) {
-			return Util.extend(matcher, names);
+		public static named(name: string, matcher: ParseFunction) {
+			return Util.extend(matcher, <NamedRule> { ruleName: name });
 		}
 
+		public static labeled(label: string, matcher: ParseFunction) {
+			return Util.extend(matcher, <NamedRule> { astName: name });
+		}
 	}
 
 	export class Grammar {
@@ -158,10 +165,13 @@ module miniup {
 			return GrammarReader.buildGrammar(ast);
 		}
 
-		registerParseRule(rule: ParseFunction) {
+		addRule(rule: ParseFunction): ParseFunction;
+		addRule(name: string, rule: ParseFunction, replace: bool = false): ParseFunction;
+		addRule(arg1: any, arg2?: ParseFunction, replace: bool = false) : ParseFunction {
+			var rule: ParseFunction = arg2 ? MatcherFactory.named(arg1, arg2) : arg1;
 			if (!rule.ruleName)
 				throw new Error("Anonymous rules cannot be registered in a grammar. ");
-			if (this.rules[rule.ruleName])
+			if (!replace && this.rules[rule.ruleName])
 				throw new Error("Rule '" + rule.ruleName + "' is already defined");
 			if ("whitespace" == rule.ruleName)
 				this.whitespaceMatcher = rule;
@@ -169,6 +179,7 @@ module miniup {
 				this.startSymbol = rule.ruleName;
 
 			this.rules[rule.ruleName] = rule;
+			return rule;
 		}
 
 		public rule(ruleName: string): ParseFunction {
@@ -205,7 +216,7 @@ module miniup {
 		currentPos: number = 0;
 		maxPos: number = -1;
 		memoizedParseFunctions = {};
-		private stack: StackItem[] = [];
+		private stack: StackItem[] = []; //TODO: is stack required anywhere?
 
 		constructor(public grammar: Grammar, public input: String) {
 
@@ -226,7 +237,7 @@ module miniup {
 
 				//TODO: left recursion support = do not if the rule is running, but has alternatives left!
 				//rules that have alternatives on recursion should catch recursion exception and move on
-				//to the next state?
+				//to the next state? (n.b. make sure that this parse method handles stack unwind properly in a finally block)
 
 				//if state is 'known' return memoized result
 				//if state is 'new' continue, set state to 'running'
@@ -281,7 +292,7 @@ module miniup {
 	}
 
 	export class ParseException {
-
+		//TODO: exception can contain detail results by looking into the last entry of the memoization cache and find all keywords / 'friendly names' / regexes
 	}
 
 	export class GrammarReader {
@@ -294,10 +305,77 @@ module miniup {
 		}
 
 		private static bootstrap(): Grammar {
+			//Based on https://github.com/dmajda/pegjs/blob/master/src/parser.pegjs
+
 			var g = new Grammar(), f = MatcherFactory;
 
+			//literals
+			var
+			  equals = f.literal("="),
+			  colon = f.literal(":"),
+			  semicolon = f.literal(";"),
+			  slash = f.literal("/"),
+			  and = f.literal("&"),
+			  not = f.literal("!"),
+			  dollar = f.literal("$"),
+			  question = f.literal("?"),
+			  star = f.literal("*"),
+			  plus = f.literal("+"),
+			  lparen = f.literal("("),
+			  rparen = f.literal(")"),
+			  dot = f.literal(".");
 
-			//TODO:
+			var
+			  identifier = f.regex(RegExpUtil.identifier),
+			  singleQuoteString = f.regex(RegExpUtil.singleQuoteString),
+			  doubleQuoteString = f.regex(RegExpUtil.doubleQuoteString),
+			  singlelinecomment = f.regex(RegExpUtil.singleLineComment),
+			  multilinecomment = f.regex(RegExpUtil.multiLineComment),
+			  whitespacechar = f.regex(RegExpUtil.whitespace),
+			  regexp = f.regex(RegExpUtil.regex),
+			  characterClass = f.regex(RegExpUtil.characterClass);
+
+			//rules
+			var seq = f.sequence, label = label, opt = f.optional;
+
+			g.addRule('grammar', label('rules', f.oneOrMore(rule)));
+
+			var str = g.addRule('string', choice(singleQuoteString, doubleQuoteString));
+			var literal = g.addRule('literal', seq(str, opt(f.literal("i"))));
+			g.addRule('whitespace', choice(whitespacechar, multilinecomment, singlelinecomment));
+
+			var rule = g.addRule('rule', seq(label('name', identifier), label('displayName', opt(str)), equals, label('expression', expression), opt(semicolon)));
+
+			var expression = g.addRule('expression', choice);
+
+			var choice = g.addRule('choice', seq(
+				label('head', sequence), label('tail', f.zeroOrMore(seq(slash, sequence)))));
+
+			var sequence = g.addRule('sequence',
+				label('elements', f.zeroOrMore(labeled)));
+
+			var labeled = g.addRule('labeled',
+			  choice(seq(label('label', identifier), colon, label('expression', prefixed)), prefixed));
+
+			var prefixed = g.addRule('prefixed', choice(
+			  seq(dollar, label('expression', suffixed)),
+			  seq(and, label('expression', suffixed)),
+			  seq(not, label('expression', suffixed)),
+			  suffixed));
+
+			var suffixed = g.addRule('suffixed', choice(
+			  seq(label('expression', primary), question),
+			  seq(label('expression', primary), star),
+			  seq(label('expression', primary), plus),
+			  primary));
+
+			var primary = g.addRule('primary', choice(
+			  seq(label('name', identifier), f.negativeLookAhead(seq(opt(str), equals)),
+			  literal,
+		      characterClass,
+		      dot,
+		      regexp,
+		      seq(lparen, label('expression', expression), rparen))));
 
 			g.startSymbol = "grammar";
 			return g;
@@ -308,10 +386,23 @@ module miniup {
 		}
 	}
 
-	export class Util {
+	export class RegExpUtil {
+		public static identifier = /[a-zA-Z_][a-zA-Z_0-9]/;
+		public static whitespace = /\s*/;
+		public static regex = /todo/;
+		public static singleQuoteString = /todo/;
+		public static doubleQuoteString = /todo/;
+		public static singleLineComment = /todo/;
+		public static multiLineComment = /todo/;
+		public static characterClass = /todo/;
+
 		public static quoteRegExp(str: string): string {
 			return (str + '').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
 		}
+	}
+
+	export class Util {
+
 
 		public static extend(thing: any, extendWith: Object): any {
 			for (var key in extendWith)
@@ -319,4 +410,6 @@ module miniup {
 			return thing;
 		}
 	}
+
+
 }
---
+++ test/recursion.js
@@ -0,0 +1,105 @@
+public static choiceMatcher(choices: ParseFunction[]): ParseFunction {
+			return (parser: Parser): any => {
+				var start = parser.currentPos;
+				var seed, seedChoice;
+				var isLeftRecursive = false;
+
+				var match = choices.some(choice => {
+					try {
+						var r = parser.parse(choice);
+						if (r !== undefined) {
+							seed = r;
+							seedChoice = choice;
+							return true;
+						}
+					}
+					catch(recursion /*TODO*/) { //TODO: are recursion exceptions allowed in the last choice?
+						isLeftRecursive = true;
+					}
+					return false;
+				}
+
+				if (!match && !isLeftRecursive)
+					return undefined;
+				else if (!match && isLeftRecursive)
+					throw recursion;
+				if (match && !isLeftRecursive)
+					return seed;
+
+				else { //handle left recursion
+					var res = seed;
+					var foundNew = false;
+
+					do {
+						choices.every(choice => {
+							//if the choice was the terminating seed choice, we're done
+							if (choice == seedChoice)
+								return false;
+
+							//memo best match till now
+							parser.memo(start, arguments.callee /* self ref to parser function */, seed);
+							parser.startPos = start; //unwind
+
+							try {
+								var r = parser.parse(choice);
+
+								//left recursive call succeeded. Store result and set up for another round
+								if (r !== undefined) {
+									res = r;
+									foundNew = true;
+									parser.memo(start, arguments.callee, r); //memoize the new match
+									return false; //this iteration done
+								}
+							}
+							catch(recursion /*TODO*/ ) {
+								//try next
+							}
+							return true;
+						})
+					} while(foundNew)
+
+					return res;
+				}
+			}
+		}
+
+
+//direct
+A = A b / b
+b = 'b'
+
+
+//indirect
+A = B x | x
+B = A y | y
+
+
+//left, priorized
+A = A '+' B | B
+B = 7
+
+//right, priorized
+A = B '+' A | B
+B = 7
+
+//generic, (left?)
+A = A '+' A | '7'
+
+A = MUL | ADD | '7'
+MUL = A '*' A
+ADD = A '+' A
+
+//left bound priorized infix op
+A = A '+'< A | '7'
+
+//right bound priorized infix op
+A = A '+'> A | '7'
+
+//other exampel
+Value   ← [0-9.]+ | '(' Expr ')'
+Product ← Expr (('*' | '/') Expr)*
+Sum     ← Expr (('+' | '-') Expr)*
+Expr    ← Product | Sum | Value
+
+//impossible
+S ← 'x' S 'x' | 'x
